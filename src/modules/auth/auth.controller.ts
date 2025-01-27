30a38a0c5cc5f6b1d712278431a9c17c7d37ba50
augustojs1 <augustojsouza1@gmail.com>
2025-01-27 20:48:44 -0300

    docs: create openAPI documentation for auth resource

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -5,11 +5,23 @@ import { SignUpDto, SignInDto, SignInSuccesDto, UserProfileDto } from './dto';
 import { AccessTokenGuard } from './guards';
 import { GetCurrentUserDecorator } from './decorators';
 import { CurrentUser } from './types';
+import { ApiBearerAuth, ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @ApiOperation({
+    summary: 'Create an account.',
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'Succesfully created a new account.',
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'User with this email already exists!',
+  })
   @Post('local/sign-up')
   public async signUpLocal(
     @Body() signUpDto: SignUpDto,
@@ -17,6 +29,17 @@ export class AuthController {
     return await this.authService.signUpLocal(signUpDto);
   }
 
+  @ApiOperation({
+    summary: 'Sign in with an existent account.',
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'Succesfully authenticated.',
+  })
+  @ApiResponse({
+    status: 403,
+    description: 'Incorrect email or password',
+  })
   @Post('local/sign-in')
   public async signInLocal(
     @Body() signInDto: SignInDto,
@@ -24,6 +47,14 @@ export class AuthController {
     return await this.authService.signInLocal(signInDto);
   }
 
+  @ApiOperation({
+    summary: 'Return profile from authenticated user.',
+  })
+  @ApiBearerAuth()
+  @ApiResponse({
+    status: 200,
+    description: 'Succesfully returned an authenticated profile.',
+  })
   @UseGuards(AccessTokenGuard)
   @Get('local/me')
   public async getMe(
