5fadccb0f315ed43cd64fa87fc36569bcdd8a7e9
augustojs1 <augustojsouza1@gmail.com>
2025-01-27 19:56:10 -0300

    docs: add openApi documentation to departments and shopping carts routes

--- src/modules/departments/departments.controller.ts
+++ src/modules/departments/departments.controller.ts
@@ -22,6 +22,12 @@ import { AccessTokenGuard, AdminGuard } from '../auth/guards';
 import { ProductsService } from '../products/products.service';
 import { PaginationParamsSortableDto } from '@/modules/common/dto';
 import { DepartmentProductsDTO } from '../products/dto';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiParam,
+  ApiResponse,
+} from '@nestjs/swagger';
 
 @Controller('departments')
 export class DepartmentsController {
@@ -31,17 +37,54 @@ export class DepartmentsController {
   ) {}
 
   @UseGuards(AccessTokenGuard, AdminGuard)
+  @ApiBearerAuth()
+  @ApiOperation({ summary: '(Admin) Create a new department.' })
+  @ApiResponse({
+    status: 201,
+    description: 'Succesfully created a new department.',
+  })
+  @ApiResponse({
+    status: 400,
+    description: `
+      - Department with this name already exists!
+      - Parent department does not exists!
+      - This department already have a parent!
+    `,
+  })
   @Post()
   public async create(@Body() data: CreateDepartmentDto) {
     return await this.departmentsService.create(data);
   }
 
   @Get()
+  @ApiOperation({ summary: 'Read all parent departments.' })
+  @ApiResponse({
+    status: 200,
+    description: 'Succesfully read all parent departments',
+  })
   public async getParentDepartments(): Promise<DepartmentDto[]> {
     return await this.departmentsService.findParentDepartments();
   }
 
   @Get(':parent_id/children')
+  @ApiOperation({
+    summary: 'Read all children departments from a parent department id.',
+  })
+  @ApiParam({
+    name: 'parent_id',
+    description: 'Id of parent department',
+    allowEmptyValue: false,
+    required: true,
+    example: '1',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Succesfully read all children departments.',
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Parent department with this id does not exists!',
+  })
   public async getChildrenDepartments(
     @GetCurrentUserDecorator() user: CurrentUser,
     @Param('parent_id') parent_id: string,
@@ -50,6 +93,30 @@ export class DepartmentsController {
   }
 
   @UseGuards(AccessTokenGuard, AdminGuard)
+  @ApiOperation({
+    summary: '(Admin) Update a department.',
+  })
+  @ApiBearerAuth()
+  @ApiParam({
+    name: 'id',
+    description: 'Id of department to be updated',
+    allowEmptyValue: false,
+    required: true,
+    example: '1',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Succesfully updated a product!',
+  })
+  @ApiResponse({
+    status: 400,
+    description: `Department with this id does not exists!
+    `,
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Department with this id does not exists!',
+  })
   @Patch(':id')
   public async update(
     @Param('id') id: string,
@@ -59,11 +126,48 @@ export class DepartmentsController {
   }
 
   @UseGuards(AccessTokenGuard, AdminGuard)
+  @ApiOperation({
+    summary: '(Admin) Delete a department.',
+  })
+  @ApiBearerAuth()
+  @ApiParam({
+    name: 'id',
+    description: 'Id of department to be delete',
+    allowEmptyValue: false,
+    required: true,
+    example: '1',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Succesfully updated a delete!',
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Department with this id does not exists!',
+  })
   @Delete(':id')
   public async delete(@Param('id') id: string): Promise<void> {
     return await this.departmentsService.delete(id);
   }
 
+  @ApiOperation({
+    summary: 'Return all products from the department id.',
+  })
+  @ApiParam({
+    name: 'id',
+    description: 'Id of products department',
+    allowEmptyValue: false,
+    required: true,
+    example: '1',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Succesfully read all products from department id!',
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Department with this id does not exists',
+  })
   @Get(':department_id/products')
   public async getProductByDepartmentId(
     @Param('department_id') department_id: string,
--- src/modules/shopping_carts/shopping_carts.controller.ts
+++ src/modules/shopping_carts/shopping_carts.controller.ts
@@ -8,6 +8,13 @@ import {
   Post,
   UseGuards,
 } from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiParam,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 
 import { ShoppingCartsService } from './shopping_carts.service';
 import { AccessTokenGuard } from '../auth/guards';
@@ -20,19 +27,33 @@ import {
   UpdateItemQuantityDTO,
 } from './dto';
 
+@ApiTags('Shopping Carts')
 @Controller('shopping-carts')
 export class ShoppingCartsController {
   constructor(private readonly shoppingCartsService: ShoppingCartsService) {}
 
   @UseGuards(AccessTokenGuard)
-  @Post()
-  public async create(
-    @GetCurrentUserDecorator() user: CurrentUser,
-  ): Promise<void> {
-    await this.shoppingCartsService.create(user.sub);
-  }
-
-  @UseGuards(AccessTokenGuard)
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Add product to shopping cart.' })
+  @ApiResponse({
+    status: 201,
+    description: 'Product successfully added to shopping cart!',
+  })
+  @ApiResponse({
+    status: 400,
+    description: `
+      - Product with this id does not exists!
+      - User shopping cart does not exists!
+      - Product is out of stock!
+      - Product is already in user shopping list!
+    `,
+  })
+  @ApiResponse({
+    status: 403,
+    description: `
+      - Users can not add their own products to a shopping cart
+    `,
+  })
   @Post('/item')
   public async createItem(
     @Body() data: CreateShoppingCartItemDto,
@@ -45,6 +66,25 @@ export class ShoppingCartsController {
   }
 
   @UseGuards(AccessTokenGuard)
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Read all products in the shopping cart.' })
+  @ApiResponse({
+    status: 200,
+    description: 'Shopping cart items succesfully returned',
+  })
+  @ApiResponse({
+    status: 404,
+    description: `
+      - Product with this id not found on user shopping cart!
+      - User shopping cart does not exists!
+    `,
+  })
+  @ApiResponse({
+    status: 400,
+    description: `
+      - Quantity not available!
+    `,
+  })
   @Get()
   public async findAll(
     @GetCurrentUserDecorator() user: CurrentUser,
@@ -53,6 +93,19 @@ export class ShoppingCartsController {
   }
 
   @UseGuards(AccessTokenGuard)
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Update shopping cart item quantity.' })
+  @ApiParam({
+    name: 'product_id',
+    description: 'Id of product to in shopping cart to update quantity.',
+    allowEmptyValue: false,
+    required: true,
+    example: 1,
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Succesfully updated shopping cart item quantity',
+  })
   @Patch('/item/product/:product_id')
   update(
     @Param('product_id') product_id: string,
@@ -67,6 +120,23 @@ export class ShoppingCartsController {
   }
 
   @UseGuards(AccessTokenGuard)
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Remove shopping cart item.' })
+  @ApiParam({
+    name: 'product_id',
+    description: 'Id of product to remove from shopping cart.',
+    allowEmptyValue: false,
+    required: true,
+    example: 1,
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Succesfully removed shopping cart item',
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Product with this id not found on user shopping cart!',
+  })
   @Delete('/item/product/:product_id')
   public async remove(
     @Param('product_id') product_id: string,
@@ -76,6 +146,24 @@ export class ShoppingCartsController {
   }
 
   @UseGuards(AccessTokenGuard)
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Create order with items from shopping cart.' })
+  @ApiParam({
+    name: 'product_id',
+    description: 'Id of product to remove from shopping cart.',
+    allowEmptyValue: false,
+    required: true,
+    example: 1,
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'Succesfully created orders from shopping cart items',
+  })
+  @ApiResponse({
+    status: 400,
+    description:
+      'An error occured when trying to create order from shopping cart item',
+  })
   @Post('/checkout')
   public async checkout(
     @GetCurrentUserDecorator() user: CurrentUser,
